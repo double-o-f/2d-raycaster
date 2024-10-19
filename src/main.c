225645ec7d0aea6575ad3952d475282ab4c5e0c0
Sam <applepatato@gmail.com>
2024-10-19 03:07:02 -0500

    scuffed 2d renderer

--- src/main.c
+++ src/main.c
@@ -9,8 +9,16 @@
 #endif
 #include <SDL2/SDL.h>
 
-#define SCREEN_WIDTH  400 // 1440// 
-#define SCREEN_HEIGHT 400 // 1080// 60
+#define SCREEN_WIDTH  800 // 1440// 1600
+#define SCREEN_HEIGHT 450 // 1080// 900
+
+#define WINDOW_WIDTH  1600 // 1440// 1600
+#define WINDOW_HEIGHT 900 // 1080// 900
+
+uint32_t voidColor = 0x00000000;
+uint32_t color0 = 0x222222FF;
+uint32_t color1 = 0x990044FF;
+uint32_t color2 = 0x004499FF;
 
 //#define MAP_SIZE 6
 //int map[MAP_SIZE * MAP_SIZE + 1] = {
@@ -24,16 +32,16 @@
 #define MAP_SIZE 7
 int map[MAP_SIZE * MAP_SIZE] = {
             1, 1, 1, 1, 1, 1, 1,
+            1, 2, 0, 0, 0, 0, 1,
             1, 0, 0, 0, 0, 0, 1,
-            1, 0, 2, 0, 2, 0, 1,
+            1, 0, 0, 2, 0, 0, 1,
             1, 0, 0, 0, 0, 0, 1,
-            1, 0, 2, 0, 2, 0, 1,
             1, 0, 0, 0, 0, 0, 1,
             1, 1, 1, 1, 1, 1, 1};
 
 //#define MAP_SIZE 8
 //int map[MAP_SIZE * MAP_SIZE] = {
-//            1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 0, 1, 1, 1,
 //            1, 0, 0, 0, 0, 0, 0, 1,
 //            1, 0, 0, 0, 0, 0, 0, 1,
 //            1, 0, 0, 0, 0, 0, 0, 1,
@@ -54,13 +62,13 @@ bool quit = false;
 struct {
     float fov; //in radians rn
     float rayStepSize; //smaller means smaller ray steps (more accurate)
+    float minHeight; //min height of wall
 } state;
 
 struct {
     float x;
     float y;
     float rot;
-    float size;
     float speed;
     float rSpeed;
 } player;
@@ -69,6 +77,40 @@ struct {
 uint32_t setColor(uint8_t R, uint8_t G, uint8_t B, uint8_t A) {
     uint32_t col = (16777216 * R) + (65536 * G) + (256 * B) + (1 * A);
     return col;
+}
+
+void playerRotate(float rSpeed) {
+    player.rot += rSpeed;
+    if (player.rot > (M_PI * 2)) {
+        player.rot = 0;
+    }
+    else if (player.rot < 0) {
+        player.rot = (M_PI * 2);
+    }
+}
+
+void playerForward(float speed) {
+    player.x += cosf(player.rot) * speed;
+    player.y += sinf(player.rot) * speed;
+}
+
+void playerbackward(float speed) {
+    player.x += cosf(player.rot) * speed * -1;
+    player.y += sinf(player.rot) * speed * -1;
+}
+
+void playerLeft(float speed) {
+    player.x += sinf(player.rot) * speed;
+    player.y += cosf(player.rot) * speed * -1;
+}
+
+void playerRight(float speed) {
+    player.x += sinf(player.rot) * speed * -1;
+    player.y += cosf(player.rot) * speed;
+}
+
+float dist(float x1, float y1, float x2, float y2) {
+    return sqrtf(fabs(x2 - x1) + fabs(y2 - y1));
 }
 
 void drawMap() {
@@ -100,13 +142,13 @@ void drawMap() {
             int wallType = map[j + (i * MAP_SIZE)];
             uint32_t screenCol;
             if (wallType == 1) {
-                screenCol = 0x990044FF;
+                screenCol = color1;
             }
             else if (wallType == 2) {
-                screenCol = 0x004499FF;
+                screenCol = color2;
             }
             else {
-                screenCol = 0x222222FF;
+                screenCol = color0;
             }
             //gapSizeY = 10;
             //gapSizeX = 10;
@@ -119,11 +161,19 @@ void drawMap() {
     }
 }
 
-void drawMapPlayer() {
-
+void drawVertLine(int x, int startY, int endY, uint32_t col) {
+    for (int i = startY; i < endY; i += 1) {
+        pixels[x + (i * SCREEN_WIDTH)] = col;
+    }
 }
 
-void drawLine(float sX, float sY, float eX, float eY, uint32_t col) {
+void fill(uint32_t col) {
+    for (int i = 0; i < (SCREEN_HEIGHT * SCREEN_WIDTH); i += 1) {
+        pixels[i] = col;
+    }
+}
+
+void drawLine(float sX, float sY, float eX, float eY, uint32_t col) {  //draw line based on map size
     float startX = (sX * SCREEN_WIDTH) / MAP_SIZE;
     float startY = (sY * SCREEN_HEIGHT) / MAP_SIZE;
     float endX = (eX * SCREEN_WIDTH) / MAP_SIZE;
@@ -185,19 +235,16 @@ void drawRays() {
         float pCos = cosf(player.rot + fovOffset + (fovStep * i));
 
         while(true) {
-            if (fabs(rayX) > MAP_SIZE || fabs(rayY) > MAP_SIZE) {
-                //pixels[i] = 0x00000000;
-                break;
-            }
+            //if (fabs(rayX) > MAP_SIZE || fabs(rayY) > MAP_SIZE) {
+            //    break;
+            //}
             int rX = (int)rayX;
             int rY = (int)rayY;
             int wallType = map[(int)rayX + ((int)rayY * MAP_SIZE)];
             if (wallType == 1) {
-                //pixels[i] = 0x990044FF;
                 break;
             }
             if (wallType == 2) {
-                //pixels[i] = 0x004499FF;
                 break;
             }
 
@@ -209,13 +256,37 @@ void drawRays() {
     }
 }
 
-void rotatePlayer(float rSpeed) {
-    player.rot += rSpeed;
-    if (player.rot > (M_PI * 2)) {
-        player.rot = 0;
-    }
-    else if (player.rot < 0) {
-        player.rot = (M_PI * 2);
+void drawWolf() {
+    float fovOffset = (state.fov / 2) * -1; //starting fov offset
+    float fovStep = (state.fov / 2) / SCREEN_WIDTH;  //move ray angle by this much every pixel
+
+    for (int i = 0; i < SCREEN_WIDTH; i += 1) {
+        float startX = player.x; //starting point of ray
+        float startY = player.y; //^^
+        float rayX = startX;
+        float rayY = startY;
+        float pSin = sinf(player.rot + fovOffset + (fovStep * i));
+        float pCos = cosf(player.rot + fovOffset + (fovStep * i));
+
+        while(true) {
+            int wallType = map[(int)rayX + ((int)rayY * MAP_SIZE)];
+            if (wallType == 1) {
+                int wallHeight = ((MAP_SIZE - dist(startX, startY, rayX, rayY)) * SCREEN_HEIGHT) / MAP_SIZE;
+                int wallStart = (SCREEN_HEIGHT - wallHeight) / 2;
+                drawVertLine(i, wallStart, wallHeight, color1);
+                break;
+            }
+            if (wallType == 2) {
+                int wallHeight = ((MAP_SIZE - dist(startX, startY, rayX, rayY)) * SCREEN_HEIGHT) / MAP_SIZE;
+                int wallStart = (SCREEN_HEIGHT - wallHeight) / 2;
+                drawVertLine(i, wallStart, wallHeight, color2);
+                break;
+            }
+
+            rayX += pCos * state.rayStepSize;
+            rayY += pSin * state.rayStepSize;
+        }
+        fovOffset += fovStep;
     }
 }
 
@@ -232,8 +303,8 @@ int main(int argc, char const *argv[])
         "test", 
         SDL_WINDOWPOS_CENTERED_DISPLAY(0), 
         SDL_WINDOWPOS_CENTERED_DISPLAY(0), 
-        800, 
-        800, //600
+        WINDOW_WIDTH, 
+        WINDOW_HEIGHT, //600
         SDL_WINDOW_ALLOW_HIGHDPI);
     if(!window) {
         fprintf(stderr, "failed to create SDL window: %s\n", SDL_GetError());
@@ -258,13 +329,13 @@ int main(int argc, char const *argv[])
     player.x = 3;
     player.y = 3;
     player.rot = 0;
-    player.size = 0.25;
     player.speed = 0.05;
     player.rSpeed = 0.05;
     
-    state.rayStepSize = 0.25;
-    state.fov = (M_PI / 2); //90
-    
+    state.rayStepSize = 0.01;
+    state.fov = ((M_PI * 2) / 6); //90
+    bool showMap = false;
+
     while (!quit){
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -275,35 +346,40 @@ int main(int argc, char const *argv[])
 
         const uint8_t *keystate = SDL_GetKeyboardState(NULL);
         if (keystate[SDL_SCANCODE_LEFT]) {
-            rotatePlayer(player.rSpeed * -1);
+            playerRotate(player.rSpeed * -1);
         }
         if (keystate[SDL_SCANCODE_RIGHT]) {
-            rotatePlayer(player.rSpeed);
+            playerRotate(player.rSpeed);
         }
-        if (keystate[SDL_SCANCODE_UP]) {
-            player.y -= player.speed;
+        if (keystate[SDL_SCANCODE_UP] || keystate[SDL_SCANCODE_W]) {
+            playerForward(player.speed);
         }
-        if (keystate[SDL_SCANCODE_DOWN]) {
-            player.y += player.speed;
+        if (keystate[SDL_SCANCODE_DOWN] || keystate[SDL_SCANCODE_S]) {
+            playerbackward(player.speed);
         }
 
         if (keystate[SDL_SCANCODE_A]) {
-            player.x -= player.speed;
+            playerLeft(player.speed);
         }
         if (keystate[SDL_SCANCODE_D]) {
-            player.x += player.speed;
-        }
-        if (keystate[SDL_SCANCODE_W]) {
-            player.y -= player.speed;
-        }
-        if (keystate[SDL_SCANCODE_S]) {
-            player.y += player.speed;
+            playerRight(player.speed);
         }
 
-        memset(pixels, 0, sizeof(pixels));
-        drawMap();
-        //drawLine(player.x, player.y, player.x, player.y, 0xFFFFFFFF);
-        drawRays();
+        if (keystate[SDL_SCANCODE_R]) {
+            if (showMap) {showMap = false;}
+            else {showMap = true;}
+        }
+
+        if (showMap) {
+            memset(pixels, 0, sizeof(pixels));
+            drawMap();
+            drawLine(player.x, player.y, player.x, player.y, 0xFFFFFFFF);
+            drawRays();
+        }
+        else {
+            fill(color0);
+            drawWolf();
+        }
 
         SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * 4);
         SDL_RenderCopyEx(
